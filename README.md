167e260ba00ad827f8bc645e3d5ce6ff13a592b8
anderer <social.depot@posteo.de>
2025-10-05 01:16:13 +0200

    fix 1passwort foo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 🏗️ andere 👷 flake 🚧
+
+nh os switch .
+nixos-rebuil switch
+
+nh os switch .#HOSTNAME --target-host HOSTNAME
+
+sops updatekeys
+
+sops edit ...
+sops decrypt ...
--- nixos/modules/workstation/1password.nix
+++ nixos/modules/workstation/1password.nix
@@ -18,9 +18,9 @@ in
       enable = true;
       polkitPolicyOwners = [ "anderer" ];
     };
-    
+
     environment.variables = {
-      SSH_AUTH_SOCK = "~/.1password/agent.sock";
+      SSH_AUTH_SOCK = "/home/anderer/.1password/agent.sock";
     };
   };
 }
