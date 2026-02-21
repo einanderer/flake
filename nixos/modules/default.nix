5750dc526721f1eb7d248b853e61cda3f5cb1139
anderer <social.depot@posteo.de>
2026-02-21 13:29:57 +0100

    change resolved settings syntax

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -182,17 +182,19 @@
     # I know how to find the nixos manual
     getty.helpLine = lib.mkForce "";
 
-    resolved = {
-      dnsovertls = "opportunistic";
-      # no google/cloudflare defaults
-      fallbackDns = [
-        "2620:fe::10#dns.quad9.net"
-        "2620:fe::fe:10#dns.quad9.net"
-        "9.9.9.10#dns.quad9.net"
-        "149.112.112.10#dns.quad9.net"
-      ];
-      # broken
-      dnssec = "false";
+    resolved.settings = {
+      "Resolve" = {
+        DNSOverTLS = "opportunistic";
+        # no google/cloudflare defaults
+        FallbackDNS = [
+          "2620:fe::10#dns.quad9.net"
+          "2620:fe::fe:10#dns.quad9.net"
+          "9.9.9.10#dns.quad9.net"
+          "149.112.112.10#dns.quad9.net"
+        ];
+        # broken
+        DNSSEC = "false";
+      };
     };
 
     journald.extraConfig = ''
